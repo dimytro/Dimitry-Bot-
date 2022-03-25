9b9f5a01989e56a4433dde4881de4136a2d310de
Dimitry <68407512+dimytro@users.noreply.github.com>
2022-03-25 10:09:22 -0500

    Update ara.js

--- plugins/ara.js
+++ plugins/ara.js
@@ -8,7 +8,7 @@ type: 'audioMessage', // paksa tanpa convert di ffmpeg
 ptt: true // true diatas ga work, sebab dipaksa tanpa convert ;v
 })
 }
-handler.customPrefix = /ara ara/i
+handler.customPrefix = /ara ara|Ara ara/i
 handler.command = new RegExp
 
 
